893d9d51584aa6f53d144ee3971e3efad8145635
Akash Bharat Kochure <115985398+akashbkochure@users.noreply.github.com>
2023-05-28 14:58:55 +0530

    Update ArgoCD.sh

--- ArgoCD.sh
+++ ArgoCD.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# For Docker Installation
-sudo apt-get install docker.io -y
-sudo usermod -aG docker $USER && newgrp docker
 
 # For Minikube & Kubectl
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
